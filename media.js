2126c9298c280e8f84cf4f85073182324d7502eb
Marcilio11-du <snooksabores@gmail.com>
2025-05-19 22:22:56 +0100

    Update

---
+++ media.js
@@ -0,0 +1,15 @@
+/*let medium = 7.5
+let mediumToPass = 10
+let message = medium < 10 ? "NÂO APTO": "APTO"
+console.log(message)*/
+
+/*Exercício 2 */
+var n1 = 14;
+var n2 = 6;
+var media = (n1+n2)/2
+console.log(`Media ${media} você, está ${media <= 10 ? "Aprovado": "Reprovado"}`);
+
+/*Exercício 3*/
+let x = 4;
+let res = x % 2 == 0 ? "PAR": "ÍMPAR"
+console.log(res);
---
+++ script.js
@@ -0,0 +1,6 @@
+var n1 = Number.parseInt(prompt("Digite o primeir número: "));
+var n2 = Number.parseFloat(prompt("Digite o segundo número: "));
+var soma = n1 + n2;
+alert("O resultado da soma é ");
+
+console.log("reuitado")
